bfe0a7a560c0380e11e11cc18e74a5e5f0508d96
dzulhilmi <dzulhilmi@users.noreply.github.com>
2021-03-02 20:56:12 +0700

    simplify content code

--- ebukusaku/src/layout/Content.jsx
+++ ebukusaku/src/layout/Content.jsx
@@ -5,10 +5,11 @@ import Divider from '@material-ui/core/Divider';
 
 const arabicRegex = /[\u0600-\u06FF]/;
 
+const text = (v, style) => v.match(arabicRegex) ? <Typography variant='body1' style={style}>{v}</Typography> : <Typography variant='body1' style={{margin: 5, fontSize: 14}}>{v}</Typography>
+
 export default function Content({data, variant='default'}) {
 
-    //type 1 untuk doa-doa seperti pada bab 
-    if(variant === '1'){
+    if(variant === '1' || variant === '2'){
         return (
             <div style={{textAlign: 'center', margin: 20}}>
                 <Typography variant='h5' style={{margin: 10, fontSize: 24}}>{data.title}</Typography>
@@ -18,24 +19,7 @@ export default function Content({data, variant='default'}) {
                 {
                     data.text.map((v, i) => (
                         <Fragment key={i}>
-                            {v.match(arabicRegex) ? <Typography variant='body1' style={{margin: 5, fontSize: 18}}>{v}</Typography> : <Typography variant='body1' style={{margin: 5, fontSize: 14}}>{v}</Typography>}
-                        </Fragment>
-                    ))
-                }
-                <img src='assets/img/pembatas.png' alt='pembatas' style={{bottom:0, left:0, marginTop: 100}}/>
-            </div>
-        );
-    } else if(variant === '2'){
-        return (
-            <div style={{textAlign: 'center', margin: 20}}>
-                <Typography variant='h5' style={{margin: 10, fontSize: 24}}>{data.title}</Typography>
-                <Typography variant='body1' style={{margin: 5}}>{data.subtitle}</Typography>
-                <Divider style={{backgroundColor: '#00af91', marginBottom: 10}}/>
-                <Typography variant='body1' style={{margin: 5, fontSize: 21}}>بِسْمِ اللهِ الرّحْمنِ الرّحِيْمِ</Typography>
-                {
-                    data.text.map((v, i) => (
-                        <Fragment key={i}>
-                            {v.match(arabicRegex) ? <Typography variant='body1' style={{margin: 5, fontSize: 18, textAlign: 'justify', textAlignLast:'right'}}>{v}</Typography> : <Typography variant='body1' style={{margin: 5, fontSize: 14}}>{v}</Typography>}
+                            {variant === '1' ? text(v, {margin: 5, fontSize: 18}) : text(v, {margin: 5, fontSize: 18, textAlign: 'justify', textAlignLast:'right'})}
                         </Fragment>
                     ))
                 }
@@ -54,11 +38,11 @@ export default function Content({data, variant='default'}) {
                                 return (
                                     <Fragment key={i}>
                                         {i === 0 ? null : <Typography variant='body1' style={{margin: 5}}>-----------</Typography>}
-                                        {v === 'بِسْمِ اللهِ الرّحْمنِ الرّحِيْمِ' ? <Typography variant='body1' style={{margin: 5, fontSize: 22}}>بِسْمِ اللهِ الرّحْمنِ الرّحِيْمِ</Typography> : v.match(arabicRegex) ? <Typography variant='body1' style={{margin: 5, fontSize: 18}}>{v}</Typography> : <Typography variant='body1' style={{margin: 5, fontSize: 14}}>{v}</Typography>}
+                                        {v === 'بِسْمِ اللهِ الرّحْمنِ الرّحِيْمِ' ? <Typography variant='body1' style={{margin: 5, fontSize: 22}}>بِسْمِ اللهِ الرّحْمنِ الرّحِيْمِ</Typography> : text(v, {margin: 5, fontSize: 18})}
                                     </Fragment>
                                 )
                             } else{
-                                return v.match(arabicRegex) ? <Typography variant='body1' style={{margin: 5, fontSize: 18}}>{v}</Typography> : <Typography variant='body1' style={{margin: 5, fontSize: 14}}>{v}</Typography>
+                                return text(v, {margin: 5, fontSize: 18})
                             }
                         })
                     }
